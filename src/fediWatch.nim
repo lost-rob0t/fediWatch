c84e05bd6c32f9cc83c2160058de2bffb65ee0fb
nsaspy <nsaspy@airmail.cc>
2023-09-16 18:31:39 -0400

    sync before threadi

--- src/fediWatch.nim
+++ src/fediWatch.nim
@@ -14,16 +14,22 @@ import strformat
 import strutils
 import cligen
 import md5
+import morelogging
+from logging import Level, LevelNames
+from os import getEnv
 type
-  FediWatch = object
+  FediWatch = ref object
     client: AsyncFediClient
     config: Target
+    lastMessage: string
+  FediWatchConfig = object
+    logpath: string
+    logLevel: string
   ResourcePool*[T] = ref object
     resources: Deque[T]
     queuers: Deque[Future[T]]
 
   AsyncHttpClientPool* = ResourcePool[AsyncHttpClient]
-
 proc dequeue*[T](pool: ResourcePool[T]): Future[T] =
   result = newFuture[T]("dequeue")
   if pool.resources.len == 0:
@@ -61,9 +67,7 @@ proc initAsyncFedi*(target: Target): AsyncFediClient =
 
 
 proc initFediWatch*(target: Target): FediWatch =
-  result.client = initAsyncFedi(target)
-  result.config = target
-
+  result = FediWatch(lastMessage: "", config: target, client: initAsyncFedi(target))
 
 proc parseFediUsername*(user: string): (string, string) =
   let data = user.split("@")
@@ -99,7 +103,7 @@ proc parseUser*(user: JsonNode, dataset: string): Username =
   result = doc
 
 
-proc handleUser(routerClient: Client, httpClient: AsyncHttpClient,  checkCache: LruCache[string, bool], userCache: LruCache[string, JsonNode], target: Target) {.async.} =
+proc handleUser(routerClient: Client, httpClient: AsyncHttpClient,  checkCache: LruCache[string, bool], userCache: LruCache[string, JsonNode], target: Target, log: FileLogger) {.async.} =
   # Handles the incoming user targets
   var
     accountID = 0
@@ -131,7 +135,7 @@ proc handleUser(routerClient: Client, httpClient: AsyncHttpClient,  checkCache: 
       userCache[target.target] = resp
       accountId = resp["id"].getInt(0)
       var doc = newUsername(resp["username"].getStr(""), domain, resp["url"].getStr(""))
-      doc.dateAdded = resp["created_at"].getStr("").parseMastoTime()
+      doc.dateAdded = resp["created_at"].getStr("").parseTime("yyyy-MM-dd'T'HH:mm:ss'.'fff'Z'", utc()).toUnix
       doc.upDateTime()
       for field in resp["fields"].getElems:
         doc.misc.add(field)
@@ -142,9 +146,12 @@ proc handleUser(routerClient: Client, httpClient: AsyncHttpClient,  checkCache: 
 
 
 
-proc processFeed(fw: FediWatch, routerClient: Client) {.async.} =
-  let timeline = await fw.client.getTimeline()
-  for data in timeline.getElems:
+proc processFeed(fw: FediWatch, routerClient: Client,  log: AsyncFileLogger) {.async.} =
+  log.info fmt"getting timeline for: {fw.config.target}"
+  let timeline = await fw.client.getTimeline(minId=fw.lastMessage)
+  var posts = timeline.getElems
+  log.info fmt"got {posts.len} posts for {fw.config.target}"
+  for data in posts:
     var smPost = SocialMPost(dataset: fw.config.dataset)
     var user = data["account"].parseUser(fw.config.dataset)
     smPost.user = user.username
@@ -160,40 +167,54 @@ proc processFeed(fw: FediWatch, routerClient: Client) {.async.} =
       let t = tag.getStr("")
       if t.len != 0:
         smPost.tags.add(t)
+    # incase its not a int?
+    fw.lastMessage = data["id"].getStr("")
     await routerClient.emit(smPost.newMessage(EventType.newDocument, routerClient.id, "SocialMPost"), EventType.newDocument)
     await routerClient.emit(user.newMessage(EventType.newDocument, routerClient.id, "Username"), EventType.newDocument)
     await routerClient.emit(newRelation(user.id, smPost.id, note = "", dataset=fw.config.dataset).newMessage(EventType.newDocument, routerClient.id, "Relation"), newDocument)
 
-proc userLoop(routerClient: Client) {.async.} =
+proc userLoop(routerClient: Client, log: AsyncFileLogger) {.async.} =
   var routerClient = routerClient
   var inbox = Message[Target].newInbox(100)
   var httpPool = newAsyncHttpClientPool(100)
   var checkCache = newLruCache[string, bool](100)
   var userCache = newLruCache[string, JsonNode](100)
   var fedis: seq[FediWatch]
-  var isReady: bool
   proc handleTarget(doc: proto.Message[Target]) {.async.} =
     let target = doc.data
     let typ = target.options{"typ"}.getStr("")
     var client = await httpPool.getHttpClient()
     case typ:
       of "Username":
-        await routerClient.handleUser(client, checkCache, userCache, target)
+        await routerClient.handleUser(client, checkCache, userCache, target, log)
       of "Domain":
         fedis.add(initFediWatch(target))
-        isReady = true
+    echo fedis.len
+    log.info(fmt"Got Target type: {typ}")
+    log.info(fmt"Target:{target.target}")
+    log.info(fmt"Target Options: {target.options}")
   inbox.registerCB(handleTarget)
   inbox.registerFilter(filterTarget)
   proto.Message[Target].withInbox(routerClient, inbox):
-      for client in fedis:
-        await client.processFeed(routerClient)
-        await sleepAsync(1500)
+      for x in 0..fedis.high:
+        try:
+          await fedis[x].processFeed(routerClient, log)
+        except Exception:
+          log.error(getCurrentExceptionMsg())
+          fedis.delete(x)
 
+      #await sleepAsync(1500)
 
 proc main(apiAddress: string = "tcp://127.0.0.1:6001", subAddress: string = "tcp://127.0.0.1:6000") =
+  let level = parseEnum[Level](getEnv("FEDIWATCH_LOG_LEVEL", "lvlInfo"))
+  var log = newAsyncFileLogger(filename_tpl=getEnv("FEDIWATCH_LOG", "$appname.$y$MM$dd.log"), flush_threshold=level)
+  log.info fmt"starRouter api address: {apiAddress}"
+  log.info fmt"starRouter pub/sub address: {subAddress}"
   var client = newClient("fediwatch", subAddress, apiAddress, 10, @[""])
   client.connect()
-  waitFor client.userLoop()
+  waitFor client.userLoop(log)
 
 when isMainModule:
   dispatch main
+
+
